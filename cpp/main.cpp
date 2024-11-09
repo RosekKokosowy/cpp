57a9348d2ae1c00334ca818e4b3ea3ffe8492367
Mateusz Cybulski <235847@edu.p.lodz.pl>
2024-11-09 13:22:37 +0100

    init'

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+
+    // 1. Zarezerwuj miejsce w pamiêci na dwie zmienne
+
+    /* Twój kod */
+
+    // 2. Wyœwietl tekst zachêcaj¹cy do wpisania danych
+
+    /* Twój kod */
+
+    // 3. Przechwyæmy tekst z konsoli od u¿ytkownika do zmiennej w pamiêci.
+
+    /* Twój kod */
+
+    // 4. Zag³êbmy siê w dzia³anie algorytmu nwd w wersji z odejmowaniem.
+
+    // 5. Rozpocznijmy pêtle, która bêdzie trwaæ tak d³ugo a¿ obie zmienne nie bêd¹ sobie równe.
+
+    /*
+        Twój kod, a w nim:
+
+        6. W zale¿noœci od tego która liczba wiêksza to odejmujemy od niej t¹ mniejsz¹ i przypisujemy dan¹ wartoœæ do wiêkszej zmiennej.
+
+    */
+
+    // 7. NWD to wartoœæ pod pierwsz¹ zmienn¹.
+
+    // 8. Teraz spróbujmy zapoznaæ siê algorytmem, ale znacznie szybszym przy u¿yciu operatora % i samemu zaimplementowaæ.
+
+
+    return 0;
+}
