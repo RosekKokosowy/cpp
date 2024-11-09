a631042c7c3c676d535ffa8bfff071ed940c3398
Mateusz Cybulski <235847@edu.p.lodz.pl>
2024-11-09 12:28:49 +0100

    init

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,1 +1,59 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+
+    // 1. Zarezerwowaæ przestrzeñ w pamiêci na wpisanie danej przez u¿ytkownika.
+
+    /* Twój kod */
+
+    // 2. Wyœwietlenie tekstu zachêcaj¹cego u¿ytkownika do wpisania danej.
+
+    /* Twój kod */
+
+    // 3. Pobranie danej u¿ytkownika z konsoli do pamiêci komputera pod zmienn¹ z kroku 1.
+
+    /* Twój kod */
+
+    // 4. Zag³êbmy siê w dzia³anie sita eratostenesa.
+
+    // 5. Wpierw stwórzmy tablicê, która bêdzie dwustanowa - albo 0 albo 1 (albo true albo false) za pomoc¹ zmiennej bool. Pamiêtaæ ¿e indeksowanie od 0.
+
+     /* Twój kod */
+
+    // 6. UStawmy true wszystkie miejsca w tablicy. Wykorzystajmy do tego pêtle for.
+
+     /* Twój kod */
+
+    // 7. Kolejna pêtla to ju¿ odsiewanie samo w sobie. Tylko jak d³ugo ma iœæ algorytm? Tak naprawdê jedynie do pierwiastka gornego przedzia³u ...
+
+     /*
+        Twój kod a w nim:
+
+        8. Sprawdzmy czy stan naszej tablicy jest true czy false (jeœli true to i jest liczb¹ pierwsz¹).
+
+        /*
+            Twój kod a w nim:
+
+            9. Jeœli jest pierwsza to oznaczmy wszystkkei wielokrtonoœci i jako nie pierwsze w tablicy czyli na false.
+
+        */
+      */
+
+      // 10. Wypisaæ liczby pierwsze od 2 do n w pêtli od 2 do n
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
