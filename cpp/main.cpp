84e0c7b1d347a4591b6b6e445e2f4e449841079f
Mateusz Cybulski <235847@edu.p.lodz.pl>
2024-11-09 11:15:24 +0100

    init

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
 
+using namespace std;
+
+/*
+int main():
+Funkcja main jest punktem startowym ka¿dego programu w C++.
+To od niej zaczyna siê wykonanie kodu. S³owo kluczowe int oznacza, ¿e funkcja zwraca liczbê ca³kowit¹.
+
+cout << "Hello, World!";:
+cout to strumieñ wyjœciowy (ang. "character output"), który s³u¿y do wypisywania tekstu na ekran.
+<< to operator przesy³ania danych do strumienia, wiêc cout << "Hello, World!"; wypisze "Hello, World!" na ekran.
+
+return:
+Na koñcu main() zazwyczaj u¿ywa siê return 0;,
+aby zakoñczyæ program i zwróciæ wartoœæ 0, co oznacza sukces.
+Jeœli zwrócimy inn¹ wartoœæ (np. return 1;), mo¿e to oznaczaæ b³¹d lub specyficzny stan programu.
+
+exit:
+Funkcja exit(int code) dzia³a podobnie do return, ale pozwala
+zakoñczyæ program z dowolnego miejsca w kodzie.
+exit(0); oznacza sukces, a exit(1); lub inny kod mo¿e oznaczaæ b³¹d.
+
+Napiszmy funkcjê startow¹ która wyœwietli napis Hello World w konsoli.
+*/
