f733de666bbfd37afb2af6e41b9e4e17afad6c03
Mateusz Cybulski <235847@edu.p.lodz.pl>
2024-11-09 11:05:30 +0100

    Init

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,1 +1,21 @@
+/*
+W C++ biblioteki to zestawy funkcji i definicji, które mo¿na zaimportowaæ do programu,
+aby nie musieæ samemu pisaæ kodu realizuj¹cego podstawowe operacje
+(np. operacje matematyczne, obs³ugê wejœcia/wyjœcia).
 
+Aby skorzystaæ z funkcji z danej biblioteki, nale¿y u¿yæ polecenia #include,
+które dodaje kod tej biblioteki do naszego programu przed jego kompilacj¹.
+Na przyk³ad:
+
+#include <iostream>
+
+Ten zapis importuje bibliotekê iostream, która jest czêœci¹ standardowej biblioteki
+C++ i zawiera funkcje do obs³ugi strumieni wejœcia i wyjœcia
+(np. std::cout do wypisywania tekstu na ekran).
+
+U¿ywanie bibliotek pomaga w tworzeniu zwiêz³ego,
+bardziej czytelnego kodu oraz unika powielania funkcji, które ktoœ ju¿ opracowa³.
+
+Podstawow¹ bibliotek¹ jest biblioteka iostream potrzebna do
+wyœwietania danych w konsoli. Postarajmy siê j¹ zaimportowaæ.
+*/
